f4d77ababa0563ac682b6178ce364d6c2d5952a0
Drew Seeholzer <drews21f7@gmail.com>
2019-06-19 14:55:41 -0600

    Initial Commit

---
+++ TaskTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskTracking
+//
+//  Created by Drew Seeholzer on 6/19/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
